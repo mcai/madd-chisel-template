891c7e550dcfb3081c0c6c53f23de3c13a31715a
Min Cai <min.cai.china@gmail.com>
2022-05-27 08:38:13 +0800

    Update project files.

--- src/main/scala/madd/MatrixAddition3IO.scala
+++ src/main/scala/madd/MatrixAddition3IO.scala
@@ -4,16 +4,12 @@ import chisel3._
 import chisel3.util._
 
 class MatrixAddition3IO(M: Int, N: Int) extends Bundle {
-  val start = Input(Bool())
-  val load = Output(Bool())
-  val done = Output(Bool())
+  val in = Flipped(DecoupledIO(new Bundle {
+    val a = SInt(32.W)
+    val b = SInt(32.W)
+  }))
 
-  val k = Output(UInt(32.W))
-
-  val a = Input(SInt(32.W))
-  val b = Input(SInt(32.W))
-
-  val out = Output(SInt(32.W))
+  val out = ValidIO(Vec(M * N, SInt(32.W)))
 
   override def cloneType =
     new MatrixAddition3IO(M, N).asInstanceOf[this.type]
