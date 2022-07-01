5083c312974e50cd809b04e8a9b57bab1bbdbbbf
Min Cai <min.cai.china@gmail.com>
2022-07-01 18:41:48 +0800

    Update cache/.

--- src/main/scala/cache/Cache1.scala
+++ src/main/scala/cache/Cache1.scala
@@ -12,116 +12,120 @@ class Meta extends Bundle with CacheConfig {
   val tag = UInt(tagBits.W)
 }
 
-class Cache1 extends Module with CacheConfig {
-    assert(assoc == 1)
+class Cache1 extends Module with CacheConfig with CurrentCycle {
+  assert(assoc == 1)
 
-    val io = IO(new CacheIO)
+  val io = IO(new CacheIO)
 
-    val dataArray = Reg(Vec(numSets, UInt((blockSizeInBytes * 8).W)))
-    val metaArray = Reg(Vec(numSets, new Meta()))
+  val dataArray = Reg(Vec(numSets, UInt((blockSizeInBytes * 8).W)))
+  val metaArray = Reg(Vec(numSets, new Meta()))
 
-    val sIdle :: sReadMiss :: sReadData :: sWriteResponse :: Nil = Enum(4)
+  val sIdle :: sReadMiss :: sReadData :: sWriteResponse :: Nil = Enum(4)
 
-    val regState = RegInit(sIdle)
+  val regState = RegInit(sIdle)
 
-    io.request.ready := false.B
+  io.request.ready := false.B
 
-    io.response.valid := false.B
-    io.response.bits := DontCare
+  io.response.valid := false.B
+  io.response.bits := DontCare
 
-    val address = io.request.bits.address
+  val address = io.request.bits.address
 
-    val tag = getTag(address)
-    val index = getIndex(address)
+  val tag = getTag(address)
+  val index = getIndex(address)
 
-    val hit = metaArray(index).valid && metaArray(index).tag === tag
+  val hit = metaArray(index).valid && metaArray(index).tag === tag
 
-    val addressReg = Reg(UInt(addressWidth.W))
-    val tagReg = getTag(addressReg)
-    val indexReg = getIndex(addressReg)
+  val addressReg = Reg(UInt(addressWidth.W))
+  val tagReg = getTag(addressReg)
+  val indexReg = getIndex(addressReg)
 
-    val memory = Module(new Memory(dataWidth, 256))
+  val memory = Module(new Memory(dataWidth, 256))
 
-    memory.io.writeEnable := false.B
-    memory.io.address := DontCare
-    memory.io.writeData := DontCare
+  memory.io.writeEnable := false.B
+  memory.io.address := DontCare
+  memory.io.writeData := DontCare
 
-    def writeback() {
-        memory.io.writeEnable := true.B
-        memory.io.address := metaArray(index).address
-        memory.io.writeData := dataArray(index)
-    }
+  def writeback() {
+      memory.io.writeEnable := true.B
+      memory.io.address := metaArray(index).address
+      memory.io.writeData := dataArray(index)
+  }
 
-    def refill() {
-        memory.io.writeEnable := false.B
-        memory.io.address := io.request.bits.address
-    }
+  def refill() {
+      memory.io.writeEnable := false.B
+      memory.io.address := io.request.bits.address
+  }
 
-    switch(regState) {
-      is(sIdle) {
-        io.request.ready := true.B
+  switch(regState) {
+    is(sIdle) {
+      io.request.ready := true.B
 
-        when(io.request.fire()) {
-          addressReg := io.request.bits.address
+      when(io.request.fire()) {
+        addressReg := io.request.bits.address
 
-          when(io.request.bits.writeEnable) {
-            when(hit) {
-              dataArray(index) := io.request.bits.writeData
+        when(io.request.bits.writeEnable) {
+          when(hit) {
+            dataArray(index) := io.request.bits.writeData
 
-              regState := sWriteResponse
-            }.otherwise {
-              when(metaArray(index).valid && metaArray(index).dirty) {
-                writeback()
-              }
-
-              metaArray(index).valid := true.B
-              metaArray(index).dirty := true.B
-              metaArray(index).tag := tag
-              metaArray(index).address := address
-              dataArray(index) := io.request.bits.writeData
-
-              regState := sWriteResponse
-            }
+            regState := sWriteResponse
           }.otherwise {
-            when(hit) {
-              regState := sReadData
-            }.otherwise {
-              when(metaArray(index).valid && metaArray(index).dirty) {
-                writeback()
-              }
-
-              refill()
-
-              regState := sReadMiss
+            when(metaArray(index).valid && metaArray(index).dirty) {
+              writeback()
             }
+
+            metaArray(index).valid := true.B
+            metaArray(index).dirty := true.B
+            metaArray(index).tag := tag
+            metaArray(index).address := address
+            dataArray(index) := io.request.bits.writeData
+
+            regState := sWriteResponse
+          }
+        }.otherwise {
+          when(hit) {
+            regState := sReadData
+          }.otherwise {
+            when(metaArray(index).valid && metaArray(index).dirty) {
+              writeback()
+            }
+
+            refill()
+
+            regState := sReadMiss
           }
         }
       }
-      is(sReadMiss) {
-        metaArray(indexReg).valid := true.B
-        metaArray(indexReg).dirty := false.B
-        metaArray(indexReg).tag := tagReg
-        metaArray(indexReg).address := addressReg
-        dataArray(indexReg) := memory.io.readData
+    }
+    is(sReadMiss) {
+      metaArray(indexReg).valid := true.B
+      metaArray(indexReg).dirty := false.B
+      metaArray(indexReg).tag := tagReg
+      metaArray(indexReg).address := addressReg
+      dataArray(indexReg) := memory.io.readData
 
-        regState := sReadData
-      }
-      is(sReadData) {
-        io.response.valid := true.B
-        io.response.bits.readData := dataArray(indexReg)
+      regState := sReadData
+    }
+    is(sReadData) {
+      io.response.valid := true.B
+      io.response.bits.readData := dataArray(indexReg)
 
-        when(io.response.fire()) {
-          regState := sIdle
-        }
-      }
-      is(sWriteResponse) {
-        io.response.valid := true.B
-
-        when(io.response.fire()) {
-          regState := sIdle
-        }
+      when(io.response.fire()) {
+        regState := sIdle
       }
     }
+    is(sWriteResponse) {
+      io.response.valid := true.B
+
+      when(io.response.fire()) {
+        regState := sIdle
+      }
+    }
+  }
+
+  chisel3.printf(
+    p"[$currentCycle] regState: ${regState}, request.fire(): ${io.request.fire()}, response.fire: ${io.response.fire()}, write: ${io.request.bits.writeEnable}, hit: ${hit}\n"
+  )
 }
 
 object Cache1 extends App {
--- src/main/scala/cache/Cache1Tester.scala
+++ src/main/scala/cache/Cache1Tester.scala
@@ -3,9 +3,57 @@ package cache
 import chisel3._
 import chisel3.iotesters.PeekPokeTester
 import chisel3.util._
+import scala.collection.mutable.ListBuffer
 
 class Cache1Tester(dut: Cache1) extends PeekPokeTester(dut) {
-    // TODO
+    // (writeEnable, address, data)
+    var trace = new ListBuffer[(Boolean, Int, Int)]()
+
+    val numLinesInTrace = 100
+
+    for (i <- 0 until numLinesInTrace) {
+      trace += ((i % 2 == 0, i, i + 1)) // TODO: generate your data here as you like
+    }
+    
+    // initialize the valid and ready bits to false
+    poke(dut.io.request.valid, false)
+    poke(dut.io.response.ready, false)
+
+    // process the lines in the trace
+    for (i <- 0 until numLinesInTrace) {
+      // wait until the request is ready
+      while (peek(dut.io.request.ready) == BigInt(0)) {
+        step(1)
+      }
+
+      // feed the request
+      poke(dut.io.request.valid, true)
+      poke(dut.io.request.bits.address, trace(i)._2)
+      poke(dut.io.request.bits.writeEnable, trace(i)._1)
+
+      // feed the write data if write is enabled
+      if (trace(i)._1) {
+        poke(dut.io.request.bits.writeData, trace(i)._3)
+      }
+
+      step(1)
+
+      // mark the request as valid and response as ready
+      poke(dut.io.request.valid, false)
+      poke(dut.io.response.ready, true)
+
+      // wait until the response is valid
+      while (peek(dut.io.response.valid) == BigInt(0)) {
+        step(1)
+      }
+      
+      // TODO: you can print or expect (validate) the response here
+
+      step(1)
+
+      // mark the response as not ready again
+      poke(dut.io.response.ready, false)
+    }
 }
 
 object Cache1Tester extends App {
