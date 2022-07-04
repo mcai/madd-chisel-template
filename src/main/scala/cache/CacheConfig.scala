e4f2158b54de88727938d0fe86b6cbc2f66592d8
Min Cai <min.cai.china@gmail.com>
2022-07-04 09:32:40 +0800

    Update CacheConfig.

--- src/main/scala/cache/CacheConfig.scala
+++ src/main/scala/cache/CacheConfig.scala
@@ -6,11 +6,12 @@ import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 
 trait CacheConfig {
     val addressWidth = 32
-    val dataWidth = 32
 
     val blockSizeInBytes = 16
     val assoc = 1 // 1~8
     val numSets = 128
+
+    val dataWidth = blockSizeInBytes * 8
 
     val capacityInBytes = blockSizeInBytes * assoc * numSets // 16 * assoc * 128 bytes = 2^4 * assoc * 2^7 bytes = 2^11 * assoc bytes = 2 * assoc kB
 
